69a766c45c548df40f4edd52a91131f15ca12f9b
Santosh Singh <superssingh@gmail.com>
2022-01-23 10:37:01 +0530

    adding scss in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+module.exports = withSass({
+  /* bydefault config  option Read For More Optios
+here https://github.com/vercel/next-plugins/tree/master/packages/next-sass
+*/
+  cssModules: true,
+});
+
 module.exports = {
   images: {
     domains: ["media.graphcms.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
