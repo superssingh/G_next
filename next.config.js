f8fd886c33663c340aefcb645cd3006bd9e09a93
Santosh Singh <superssingh@gmail.com>
2022-01-23 11:27:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 const path = require("path");
-const withSass = require("@zeit/next-sass");
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-here https://github.com/vercel/next-plugins/tree/master/packages/next-sass
-*/
-  cssModules: true,
-});
 
 module.exports = {
   images: {
     domains: ["media.graphcms.com"],
   },
+};
+
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
