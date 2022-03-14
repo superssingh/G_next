1682533f994f9da69994fba2b3aaf175baf2992b
Santosh Singh <superssingh@gmail.com>
2022-03-14 23:50:02 +0530

    updates

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -29,7 +29,7 @@ const Home = ({ posts, categories }) => {
           ))}
         </div>
         <div className="grid w-full md:col-span-8 lg:col-span-4">
-          <div className="grid w-full bg-slate-300 md:flex lg:grid lg:sticky place-content-center">
+          <div className=" md:flex lg:grid lg:sticky place-content-center">
             <PostWidget recentPosts={blogs} />
             <Categories categories={categories} />
           </div>
