6b0e553d6f13d46f8a00797391cfe5f2b1750c94
Santosh Singh <superssingh@gmail.com>
2022-06-25 02:17:10 +0530

    updates

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,17 +3,14 @@
 const About = () => {
   return (
     <div className=" w-full items-center bg-white/20 shadow-lg p-4 rounded-md">
-      <div>Page under process...</div>
-      {/* <Image
-          alt={post.author.name}
-          height="30px"
-          width="30px"
-          className="shadow-xl bg-slate-300 rounded-full"
-          src={post.author.photo.url}
-        />
-        <p className="inline pl-2 pr-2 text-blue-700 cursor-pointer">
-          {post.author.name}
-        </p> */}
+      <div>About Us</div>
+      <div className="relative ">
+        Hello, welcome to gogixer. An online tech and web development website
+        for web developers, bloggers, and content creators. Hope our content
+        help you to fulfil your desired outcome 💎.
+        {/* I am Santosh Singh. I am the founder of gogixer and a web
+        developer by profession. */}
+      </div>
     </div>
   );
 };
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,49 @@
+import { TextField } from "@mui/material";
+
 const Contact = () => {
-  return <div>Coming soon... </div>;
+  return (
+    <div className="grid place-content-center">
+      <div className="grid place-content-center m-2 p-4  transition-all duration-700 bg-white  text-gray-500 rounded-lg border-2 border-solid shadow-lg ">
+        <div>Contact Us</div>
+        <div className="flex p-2 ">
+          <div className="m-2 ">
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              className="m-4"
+            />
+          </div>
+          <div className="m-2 ">
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              type="email"
+              className="m-4"
+            />
+          </div>
+        </div>
+        <div className=" m-2 ">
+          <div className="m-2">
+            <TextField
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={4}
+              fullWidth
+              className="mx-4"
+            />
+          </div>
+        </div>
+        <div className="grid place-content-center">
+          <button type="submit" className="leafButton">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
