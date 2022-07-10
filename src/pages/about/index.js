f14bbaf2ca21b1f33ce8b92156919a026c879814
Santosh Singh <superssingh@gmail.com>
2022-07-10 23:52:45 +0530

    Update about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,14 +3,15 @@
 const About = () => {
   return (
     <div className="grid w-full place-content-center p-2 ">
-      <div className="w-fit max-w-2xl place-items-center shadow-gray-500  shadow-md p-2 rounded-md bgDefault showFromTop">
-        <div className=" text-xl font-semibold w-fit">About Us</div>
-        <div className="relative p-2">
-          Hello, welcome to gogixer. An online tech and web development website
-          for web developers, bloggers, and content creators. Hope our content
-          help you to fulfil your desired outcome 💎.
-          {/* I am Santosh Singh. I am the founder of gogixer and a web
-        developer by profession. */}
+      <div className="grid w-fit max-w-2xl place-items-center shadow-gray-500 p-2 shadow-md rounded-md bgDefault showFromTop">
+        <div className="text-xl text-center font-semibold w-fit p-2">
+          About Us
+        </div>
+        <div className="relative p-2 ">
+          <h1>Hellllooooo, </h1>
+          Welcome to gogixer. An online tech and web development website for web
+          developers, bloggers, and content creators. Hope our content help you
+          to fulfil your desired outcome 💎.
         </div>
       </div>
     </div>
