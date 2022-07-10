8b7604b1e14b08fb11fda4a591fb1709243daeea
Santosh Singh <superssingh@gmail.com>
2022-07-10 18:57:10 +0530

    Update index.js

--- src/pages/author/index.js
+++ src/pages/author/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { AuthorInfo } from "../../components";
+// import { AuthorInfo } from "../../components";
 const Author = ({ author }) => {
-  return (
-    <div>
-      <AuthorInfo author={""} />
-    </div>
-  );
+  return <div>{/* <AuthorInfo author={""} /> */}</div>;
 };
 
 export default Author;
