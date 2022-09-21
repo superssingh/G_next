e1f3d3d917aed38207132c5f6ffcfd3ec97629b1
Santosh Singh <superssingh@gmail.com>
2022-09-21 11:21:06 +0530

    ui

--- src/components/jsx/HeroPage.jsx
+++ src/components/jsx/HeroPage.jsx
@@ -1,17 +1,15 @@
 import Image from "next/image";
-import * as TagName from "../js/constants";
-import styles from "../js/styles";
 import { coding, heroImg } from "../assets";
 
 const HeroPage = () => {
   return (
     <div>
-      <div className="flex-1 flex justify-center items-center md:my-0 my-10 relative transition-all duration-500 p-8 hover:bg-opacity-50 showFromTop ">
+      <div className="grid justify-center items-center md:my-0 my-10 relative transition-all duration-500 p-4 showFromTop ">
         <div className="relative flex md:justify-center items-center text-slate-100">
-          <div className={` md:flex text-center md:my-0 my-10 relative`}>
-            <span className="p-6 ">
-              <p className=" text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-300 via-red-200 to-orange-300">
-                👋 Hello their
+          <div className=" md:flex text-center md:my-0 my-10 relative">
+            <span className=" ">
+              <p className=" text-4xl text-transparent bg-clip-text bg-gradient-to-r from-orange-300 via-red-200 to-orange-300">
+                👋 Hello
               </p>
               <p className="mt-4 text-3xl pb-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-red-200 to-violet-500">{`{ Learn by doing. }`}</p>
               <div className="text-transparent bg-clip-text bg-gradient-to-r from-orange-300 via-red-100 to-pink-300 text-xl text-center md:text-left w-96 rounded-lg p-4  ">
@@ -19,13 +17,14 @@ const HeroPage = () => {
                 gives you hands-on experience in development.
               </div>
             </span>
-
-            <Image
-              src={coding}
-              width={400}
-              height={300}
-              className={`absolute rounded-xl z-[2] `}
-            />
+            <div className="w-full sm-60 md:64">
+              <Image
+                src={coding}
+                width={300}
+                height={250}
+                className={`absolute rounded-xl z-[2] `}
+              />
+            </div>
 
             {/* <coding /> */}
 
