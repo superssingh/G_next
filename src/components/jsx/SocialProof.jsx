503ceb414a5f86059e86ccf0fcbfc9ad20c8d061
Santosh Singh <superssingh@gmail.com>
2022-09-21 13:21:36 +0530

    ui changes

--- src/components/jsx/SocialProof.jsx
+++ src/components/jsx/SocialProof.jsx
@@ -8,7 +8,7 @@ const SocialProof = () => {
         <div className="grid relative w-full sm:grid-cols-2 md:grid-cols-3 gap-4 place-items-center place-content-center">
           {TagName.reviews.map((r) => {
             return (
-              <div className="grid relative w-72 sm:w-60 md:w-60 lg:w-80 mt-2">
+              <div className="grid relative w-76 sm:w-60 md:w-60 lg:w-80 mt-2">
                 <div className="ReviewerPic grid absolute h-16 w-16 p-1 left-0 top-[-14px] rotate-45 rounded-full overflow-hidden z-[1]">
                   <Image
                     className=" h-wrap w-wrap rounded-full rotate-[-45deg] "
