c1531896efee6af48c08fce9df8c8575a502f7b2
Santosh Singh <superssingh@gmail.com>
2022-11-13 04:52:19 +0530

    Update Layout.jsx

--- src/components/jsx/Layout.jsx
+++ src/components/jsx/Layout.jsx
@@ -1,35 +1,19 @@
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
+import React from "react"
+import dynamic from "next/dynamic"
 
 const Navbar = dynamic(() => import("../jsx/Navbar"), {
   ssr: false,
-});
+})
 
-const Layout = ({ children, posts }) => {
-  const [cart, setCart] = useState([]);
-  const [user, setUser] = useState([]);
-
-  const handleShop = (items) => {
-    setShop(items);
-  };
-
+const Layout = ({ children }) => {
   return (
     <React.StrictMode>
       <div className="containerBox flex flex-col min-w-screen min-h-screen ">
-        {/* <BlogContext.Provider
-          value={{
-            blogs: blogs,
-            categories: categories,
-            setBlogs: handleBlogs,
-            setCategories: handleCategory,
-          }}
-        > */}
         <Navbar />
         <div>{children}</div>
-        {/* </BlogContext.Provider> */}
       </div>
     </React.StrictMode>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
