b36894007cb7b02fa29c147694df7b43136176a4
Santosh Singh <superssingh@gmail.com>
2023-01-11 23:50:30 +0530

    ui changes

--- src/components/jsx/NotFound.jsx
+++ src/components/jsx/NotFound.jsx
@@ -4,16 +4,16 @@ const NotFound = ({ message, returnPath }) => {
   return (
     <div
       className="showSlow  
-      grid relative w-full place-content-center p-2 pt-4"
+      grid relative w-full place-content-center p-2 pt-4 "
     >
-      <div className="px-4 py-8 animate-pulse text-xl font-extrabold  text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-red-500 m-4 shadow-md  ">
-        {message || "Not Found!"}
+      <div className="px-4 py-8 animate-pulse text-xl font-extrabold  text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-red-500 m-4 bg-black/30 shadow-md shadow-black ">
+        {message || 'Not Found!'}
       </div>
       <div className="">
         <Back path={returnPath} />
       </div>
     </div>
-  )
+  );
 }
 
 export default NotFound
--- src/pages/courses/ui.js
+++ src/pages/courses/ui.js
@@ -2,9 +2,12 @@ import { NotFound } from "../../components"
 const UI = () => {
   return (
     <div className="grid relative ">
-      <NotFound message="{ Coming soon... }" returnPath="/courses" />
+      <NotFound
+        message="{ Coming soon... }"
+        returnPath="/courses"
+      />
     </div>
-  )
+  );
 }
 
 export default UI
