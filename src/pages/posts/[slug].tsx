c048b72ea49446e218a6eebade91ac0508ea4b83
Santosh Singh <superssingh@gmail.com>
2023-03-26 23:08:39 +0530

    Update [slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ const Post = () => {
             <div className="relative grid w-full place-content-center gap-x-6 gap-y-4 pb-4 md:px-4 lg:grid-cols-12 ">
               <div className=" grid max-w-4xl content-center place-self-center md:col-span-8 ">
                 <PostDetail post={post.data} />
-                <Comment />
+                {/* <Comment  /> */}
               </div>
               <div className="grid w-full md:col-span-8 lg:col-span-4 lg:place-content-start place-items-center">
                 <div className="place-content-center md:flex lg:sticky lg:grid lg:place-content-start">
