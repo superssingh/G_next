d490e8c54dc8b21418ecdd5698563b680edb4b99
Santosh Singh <superssingh@gmail.com>
2023-04-10 04:00:11 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
   },
 }
