5c6b06d22fbf7e6dd5c1603f5f321a089bc823a3
Santosh Singh <superssingh@gmail.com>
2023-04-11 09:52:29 +0530

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,5 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     // '@typescript-eslint/no-unused-vars': 'off',
     // 'react/no-unescaped-entities': 'off',
-    // '@next/next/no-page-custom-font': 'off',
   },
 }
